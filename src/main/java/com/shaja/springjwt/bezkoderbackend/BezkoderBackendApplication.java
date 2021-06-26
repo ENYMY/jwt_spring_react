3bd2a813bf8be82923786e3e2fce3fd448fe3e8c
Shaja Nasir <Shajanasir@gmail.com>
2021-06-26 10:02:43 +0530

    initial setup

---
+++ src/main/java/com/shaja/springjwt/bezkoderbackend/BezkoderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shaja.springjwt.bezkoderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BezkoderBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BezkoderBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaja/springjwt/bezkoderbackend/BezkoderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaja.springjwt.bezkoderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezkoderBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
